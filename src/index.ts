7d501cf39fa3efb3d3cf5553118126b0c485108d
liuhuanwen <liuhuanwen@zuinianqing.com>
2019-08-21 15:50:34 +0800

    feat：对url的基本处理

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import { AxiosRequestConfig } from './types/init'
+import { buildURL } from './helpers/url'
 import xhr from './xhr'
 function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
 }
 
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformUrl(config)
+}
+
+function transformUrl(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
+}
 export default axios
